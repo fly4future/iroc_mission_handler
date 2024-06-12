85d97ba06b676fc1a206aea5dfefe49336bc61fb
Vojtech Spurny <spurny@fly4future.com>
2024-06-12 17:28:19 +0200

    add action msg and handler for accepting and preempting

--- src/mission_manager.cpp
+++ src/mission_manager.cpp
@@ -4,9 +4,14 @@
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
 
+#include <actionlib/server/simple_action_server.h>
+#include <mrs_mission_manager/waypointMissionAction.h>
+
 #include <mrs_lib/param_loader.h>
 #include <mrs_lib/mutex.h>
 #include <mrs_lib/subscribe_handler.h>
+
+#include <atomic>
 
 //}
 
@@ -22,6 +27,20 @@ namespace mrs_mission_manager
 
   private:
     ros::NodeHandle nh_;
+
+    std::atomic_bool have_new_goal_ = false;
+    std::atomic_bool is_initialized_ = false;
+
+    // | --------------------- actionlib stuff -------------------- |
+    //
+
+    typedef actionlib::SimpleActionServer<mrs_mission_manager::waypointMissionAction> MissionManagerServer;
+    void                                                                              actionCallbackGoal();
+    void                                                                              actionCallbackPreempt();
+    std::unique_ptr<MissionManagerServer>                                             mission_manager_server_ptr_;
+
+    static const mrs_mission_manager::waypointMissionGoal _ACTION_SERVER_GOAL_;
+    mrs_mission_manager::waypointMissionGoal              action_server_goal_;
   };
   //}
 
@@ -44,13 +63,62 @@ namespace mrs_mission_manager
       ROS_ERROR("[MissionManager]: Could not load all parameters!");
       ros::shutdown();
     }
-
+    mission_manager_server_ptr_ = std::make_unique<MissionManagerServer>(nh_, ros::this_node::getName(), false);
+    mission_manager_server_ptr_->registerGoalCallback(boost::bind(&MissionManager::actionCallbackGoal, this));
+    mission_manager_server_ptr_->registerPreemptCallback(boost::bind(&MissionManager::actionCallbackPreempt, this));
+    mission_manager_server_ptr_->start();
 
     // | --------------------- finish the init -------------------- |
 
     ROS_INFO("[MissionManager]: initialized");
     ROS_INFO("[MissionManager]: --------------------");
+    is_initialized_ = true;
   }
+
+  // | ---------------------- action server callbacks --------------------- |
+
+  /*  actionCallbackGoal()//{ */
+
+  void MissionManager::actionCallbackGoal() {
+
+    have_new_goal_ = false;
+
+    boost::shared_ptr<const mrs_mission_manager::waypointMissionGoal> temp = mission_manager_server_ptr_->acceptNewGoal();
+
+    ROS_INFO("[MissionManager]: got a new goal from the action server");
+
+    if (!is_initialized_) {
+      mrs_mission_manager::waypointMissionResult  action_server_result;
+      action_server_result.success = false;
+      action_server_result.message = "Not initialized yet";
+      ROS_ERROR("[MrsActionlibInterface]: not initialized yet");
+      mission_manager_server_ptr_->setAborted(action_server_result);
+      return;
+    }
+
+
+    // TODO: check that th goal is valid
+
+    have_new_goal_ = true;
+    action_server_goal_ = *temp;
+  }
+
+  //}
+
+  /*  actionCallbackPreempt()//{ */
+
+  void MissionManager::actionCallbackPreempt() {
+
+    if (mission_manager_server_ptr_->isActive()) {
+
+      ROS_INFO_ONCE("[MissionManager]: preempted");
+
+      // abort the mission
+      mission_manager_server_ptr_->setPreempted();
+    }
+  }
+
+//}
 
   //}
 
